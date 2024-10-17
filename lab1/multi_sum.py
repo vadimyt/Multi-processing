c6eab0997cbcc6061146baeca81bfae93f67efef
vadimyt <pvcs@yandex.ru>
2024-10-17 04:07:43 +0300

    full remake with numpy

--- lab1/multi_sum.py
+++ lab1/multi_sum.py
@@ -1,60 +1,142 @@
 import multiprocessing
+import numpy as np
+from contextlib import closing
+import os
 
+def __init(shared_arr_):
+    global shared_arr
+    shared_arr = shared_arr_
 
-def sum(arr:list):
-    copy = arr.copy()
-    sum = copy.pop()
-    for val in copy:
-        sum+=val
+def __shared_to_numpy(shared_arr, dtype, shape):
+    return np.frombuffer(shared_arr, dtype=dtype).reshape(shape)
+
+def __create_shared_array(dtype, shape):
+    dtype = np.dtype(dtype)
+    cdtype = np.ctypeslib.as_ctypes_type(dtype)
+    shared_arr = multiprocessing.RawArray(cdtype, sum(shape))
+    arr = __shared_to_numpy(shared_arr, dtype, shape)
+    return shared_arr, arr
+
+def __create_shared_array_from_array(array:np.ndarray):
+    shared_arr, arr = __create_shared_array(array.dtype, array.shape)
+    for i,val in enumerate(array):
+        shared_arr[i]=val
+    return shared_arr, arr
+
+def __shared_sum(tuple):
+    x, y = tuple
+    arr = __shared_to_numpy(*shared_arr)
+    arr[x]=arr[x]+arr[y]
+
+def __generate_layer_pairs(j):
+    layer = []
+    i = 0
+    while i<j:
+        layer.append((i,j))
+        i+=1
+        j-=1
+    return layer
+
+def sum_array(arr:np.ndarray):
+    sum:int = 0
+    for val in arr:
+        sum += val
     return sum
 
-def threaded_sum(arr:list, workers = 2):
-    copy = arr.copy()
-    while(len(copy) != 1):
-        localArr = []
-        if len(copy)%2!=0:
-            localArr.append([0,copy.pop()])
-        for i in range(0,len(copy),2):
-            localArr.append([copy[i],copy[i+1]])
-        pool_obj = multiprocessing.Pool(workers) 
-        results = pool_obj.map(sum, localArr)
-        pool_obj.close()
-        pool_obj.join()
-        copy = results
-    return(copy.pop())
+def parallel_sum_array_piramid(arr:np.ndarray, workers = os.cpu_count()):
+    shared_arr, local_arr = __create_shared_array_from_array(arr)
+    n = local_arr.size
+    while(n!=1):
+        j=n-1
+        with closing(multiprocessing.Pool(
+            workers, initializer=__init, initargs=((shared_arr, local_arr.dtype, local_arr.shape),))) as p:
+            p.map(__shared_sum, __generate_layer_pairs(j))
+        p.join()
+        n = int((n+1)/2)
+    return local_arr[0]
 
-def generate_list(n:int, a=-100, b=100):
-    import random
-    list = []
-    for i in range(n):
-        list.append(random.randint(a,b))
-    return list
+def task(array_size = 10, workers = os.cpu_count()):
+    import time
+    multiprocessing.freeze_support()
+
+    start_time = time.time()
+    arr = np.random.randint(100,size=array_size)
+    arr.astype(np.int64)
+    end_time = time.time()
+    print("Time took to generate array of size %i:" % (array_size))
+    print("--- %s seconds ---" % (end_time - start_time))
+
+    print("Initial array: ")
+    print(arr)
+
+    print('\n')
+
+    start_time = time.time()
+    print(arr.sum())
+    end_time = time.time()
+    print("Numpy built-in sum:")
+    print("--- %s seconds ---" % (end_time - start_time))
+
+    start_time = time.time()
+    print(sum_array(arr))
+    end_time = time.time()
+    print("Simple sum:")
+    print("--- %s seconds ---" % (end_time - start_time))
+
+    start_time = time.time()
+    print(parallel_sum_array_piramid(arr, workers))
+    end_time = time.time()
+    timeA = end_time - start_time
+    print("Parallel sum:")
+    print("--- %s seconds ---" % (end_time - start_time))
+
+    print('\n')
+
+    return timeA
+
+def create_test_args(start_array_size = 100000, stop_array_size = 1000000, step = None, max_workers = os.cpu_count()):
+    args = []
+    if (step == None):
+        step = start_array_size
+    for array_size in range(start_array_size,stop_array_size+step,step):
+        for workers in range(1,max_workers+1):
+            args.append((array_size, workers))
+    return args
+
+def graph():
+    import math
+    from matplotlib import pyplot as plt
+    args = create_test_args(100, 300, max_workers=2)
+    x = []
+    y = []
+    z = {}
+    for array_size,workers in args:
+        x.append(array_size)
+        y.append(workers)
+        z[(array_size,workers)]=task(array_size,workers)*100
+    
+    xgrid, ygrid = np.meshgrid(x, y)
+    zgrid = []
+    for j in range(len(xgrid)):
+            zgrid.append([])
+    for k in range(len(zgrid)):
+        for l in range(len(xgrid[k])):
+            try:
+                zgrid[k].append(z[(xgrid[k][l],ygrid[k][l])])
+            except:
+                zgrid[k].append(np.NaN)
+
+    fig, ax = plt.subplots()
+    vmin = math.floor(z[min(z)])
+    vmax = math.ceil(z[max(z)])
+    num = 100
+    contourf_ = ax.contourf(xgrid, ygrid, zgrid, levels=np.linspace(vmin,vmax,num),extend='max')
+    cbar = fig.colorbar(contourf_,ticks=range(vmin, vmax+1, 10))
+    t = fig.get_axes()[0].axes
+    t.set_xlabel("Array size")
+    t.set_ylabel("Workers")
+    fig.show()
+    input()
 
 if __name__ == "__main__":
-    import time
-    array = generate_list(1000)
-
-    start_time = time.time()
-    print(sum(array))
-    end_time = time.time()
-    print("--- %s seconds ---" % (end_time - start_time))
-
-    # start_time = time.time()
-    # print(threaded_sum(array))
-    # end_time = time.time()
-    # print("--- %s seconds ---" % (end_time - start_time))
-
-    # start_time = time.time()
-    # print(threaded_sum(array, 4))
-    # end_time = time.time()
-    # print("--- %s seconds ---" % (end_time - start_time))
-
-    # start_time = time.time()
-    # print(threaded_sum(array, 8))
-    # end_time = time.time()
-    # print("--- %s seconds ---" % (end_time - start_time))
-
-    start_time = time.time()
-    print(threaded_sum(array, 16))
-    end_time = time.time()
-    print("--- %s seconds ---" % (end_time - start_time))
+    task()
